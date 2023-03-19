c6c3763aed42d2bab5d40de611249aed3290bf2c
rlawodnr1109 <wodnr1109@naver.com>
2023-03-20 03:35:15 +0900

    feat: 컴포넌트 파일구조 세팅

---
+++ src/components/filterIsOrder/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function FilterIsOrder() {
+  return <div>FilterIsOrder</div>
+}
+
+export default FilterIsOrder
---
+++ src/components/sortData/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SortData() {
+  return <div>SortData</div>
+}
+
+export default SortData
