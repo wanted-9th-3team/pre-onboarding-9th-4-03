09da8f000a249f2f31853e63f25ce23bfacb5012
skyhanull <anky0425@naver.com>
2023-03-20 23:51:03 +0900

    feat: 버튼이 아니고 토글로 필터링구현

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,4 +1,15 @@
-import { Table, Thead, Tbody, Tfoot, Tr, Th, Td } from '@chakra-ui/react'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  Select,
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Ttable } from '../../Type'
 
 interface ITableList {
@@ -6,13 +17,66 @@ interface ITableList {
 }
 
 function TableItem({ tableData }: ITableList) {
+  const [sortName, setSortName] = useState('오름차순')
+  const [timeName, setTimeName] = useState('오름차순')
+  const nameArray = ['오름차순', '내림차순']
+  const idurl = 'sort'
+  const timeurl = 'timeAndDate'
+  const navigate = useNavigate()
+
+  const orderChangeHandler = (value: any) => {
+    if (value === '오름차순') {
+      navigate(`/sort`)
+      setSortName('내림차순')
+    } else {
+      navigate('/')
+      setSortName('오름차순')
+    }
+  }
+
+  const timeChangeHandler = (value: any) => {
+    if (value === '오름차순') {
+      navigate(`/timeAndDate`)
+      setTimeName('내림차순')
+    } else {
+      navigate('/')
+      setTimeName('오름차순')
+    }
+  }
+
   return (
     <Table variant="striped" colorScheme="teal">
       <Thead>
         <Tr>
-          <Th>To convert</Th>
-          <Th>into</Th>
-          <Th isNumeric>multiply by</Th>
+          <Th>
+            <Select
+              // placeholder="주문번호"
+              onChange={() => orderChangeHandler(sortName)}
+              value={sortName}
+            >
+              {/* <option value="오름차순">오름차순</option>
+            <option value="내림차순">내림차순</option> */}
+              {nameArray.map(item => (
+                <option value={item} key={item}>
+                  {item}
+                </option>
+              ))}
+            </Select>
+          </Th>
+          <Th>가격</Th>
+          <Th>고객 번호</Th>
+          <Th>고객 이름</Th>
+          <Th>주문처리 상태</Th>
+          <Th>
+            <Select
+              // placeholder="주문번호"
+              onChange={() => timeChangeHandler(timeName)}
+              value={timeName}
+            >
+              <option value="오름차순">오름차순</option>
+              <option value="내림차순">내림차순</option>
+            </Select>
+          </Th>
         </Tr>
       </Thead>
       <Tbody>
