6b338fbcc4125a1b64cdd6284051f44219802f51
rlawodnr1109 <wodnr1109@naver.com>
2023-03-20 16:59:36 +0900

    feat: 필터 기능 구현. But Sorting과 연동 X.

--- src/components/filterIsOrder/index.tsx
+++ src/components/filterIsOrder/index.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+/* eslint-disable react/destructuring-assignment */
+import { Box, Button } from '@chakra-ui/react'
+import { useSearchParams } from 'react-router-dom'
 
 function FilterIsOrder() {
-  return <div>FilterIsOrder</div>
+  const [searchParams, setSearchParams] = useSearchParams()
+  const sortParams = searchParams.get('sortParams')
+
+  const filterHandler = (data: string) => {
+    if (sortParams !== null) {
+      searchParams.set('filterParams', data)
+      searchParams.set('sortParams', sortParams)
+      setSearchParams(searchParams)
+    } else {
+      searchParams.set('filterParams', data)
+      searchParams.set('sortParams', 'sortASC')
+      setSearchParams(searchParams)
+    }
+  }
+
+  return (
+    <Box>
+      주문 처리 상태 :
+      <Button
+        onClick={() => {
+          filterHandler('all')
+        }}
+      >
+        전체
+      </Button>
+      <Button
+        onClick={() => {
+          filterHandler('O')
+        }}
+      >
+        O
+      </Button>
+      <Button
+        onClick={() => {
+          filterHandler('X')
+        }}
+      >
+        X
+      </Button>
+    </Box>
+  )
 }
 
 export default FilterIsOrder
