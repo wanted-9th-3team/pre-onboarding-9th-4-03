c3e19ce243126dbfe5e627b79192465f5cc1451a
skyhanull <anky0425@naver.com>
2023-03-20 23:46:26 +0900

    refactor: 개발을 위해 delay시간 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { useEffect } from 'react'
-import { useRecoilState } from 'recoil'
+import { useSetRecoilState } from 'recoil'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import './App.css'
 import getTableInfo from './apis/TableApi.ts'
 import { ContentData } from './atom'
 import { Ttable } from './Type'
+import useInterval from './utils/util'
 
 function App() {
-  const [tableData, setTableData] = useRecoilState(ContentData)
+  const setTableData = useSetRecoilState(ContentData)
+  const delay = 500000
 
   const dataHandler = async () => {
     const response: Ttable[] = await getTableInfo()
@@ -24,7 +26,10 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  console.log(tableData)
+  useInterval(() => {
+    dataHandler()
+  }, delay)
+
   return (
     <BrowserRouter>
       <Routes>
