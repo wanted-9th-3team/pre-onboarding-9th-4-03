4cf3aa0bfbb8e4e7662dfb9331952279fd6c19ed
michoball <myunghun0114@gmail.com>
2023-03-20 21:37:45 +0900

    refactor: Apply TableControl and TableView components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,17 @@
-function Home() {
-  return <h1>Hello World</h1>
-}
+import { Box, Center } from '@chakra-ui/react'
+import TableControl from '../../components/TableControl'
+import TableView from '../../components/TableView'
 
+function Home() {
+  return (
+    <>
+      <Box h="calc(10vh - 20px)" display="flex" justifyContent="space-between">
+        <TableControl />
+      </Box>
+      <Center w="100%">
+        <TableView />
+      </Center>
+    </>
+  )
+}
 export default Home
