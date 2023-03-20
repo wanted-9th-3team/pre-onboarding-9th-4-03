47912717d6b643203c491c256b1a793c992dc50e
skyhanull <anky0425@naver.com>
2023-03-20 12:57:50 +0900

    feat:동적라우터 추가및 useeffect로 데이터 패칭

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useRecoilState } from 'recoil'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import './App.css'
+import getTableInfo from './apis/TableApi.ts'
+import { ContentData } from './atom'
+import { Ttable } from './Type'
 
 function App() {
+  const [tableData, setTableData] = useRecoilState(ContentData)
+
+  const dataHandler = async () => {
+    const response: Ttable[] = await getTableInfo()
+    const filteredRes = response.filter(
+      el => el.transaction_time.split(' ')[0] === '2023-03-08'
+    )
+    setTableData(filteredRes)
+    return response
+  }
+  useEffect(() => {
+    dataHandler()
+  }, [])
+
+  console.log(tableData)
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}>
+          <Route path="/:name" element={<Home />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
