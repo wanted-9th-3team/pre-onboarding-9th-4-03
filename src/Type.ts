74cf124d364d0569ff45dfe85f9a03de3ee52609
michoball <myunghun0114@gmail.com>
2023-03-20 21:36:18 +0900

    feat: Add table data type

--- src/Type.ts
+++ src/Type.ts
@@ -1,4 +1,18 @@
-export interface Ttable {
-  id: string
-  value: number
+export interface ITable {
+  id: number
+  status: boolean
+  currency: string
+  customer_id: number
+  customer_name: string
+  transaction_time: string
+}
+
+export type TSortingCategory =
+  | keyof Pick<ITable, 'id' | 'transaction_time'>
+  | 'reset'
+
+export enum SORTCATEGORY {
+  SORT_ID = 'id',
+  SORT_TRANSACTION = 'transaction_time',
+  SORT_RESET = 'reset',
 }
