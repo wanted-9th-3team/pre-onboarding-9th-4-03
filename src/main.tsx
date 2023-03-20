1df7a9ecde755e2c06d8678457bd51389201dd53
michoball <myunghun0114@gmail.com>
2023-03-20 21:35:57 +0900

    feat: Apply react-query, redux-toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import './index.css'
+import { store } from './store'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const queryClient = new QueryClient()
+
+root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </Provider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 )
