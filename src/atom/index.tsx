34b76c3debadb205454af28dbd08fb4ab4b22909
skyhanull <anky0425@naver.com>
2023-03-20 13:36:12 +0900

    feat: atom생성

---
+++ src/atom/index.tsx
@@ -0,0 +1,17 @@
+import { atom } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
+import { Ttable } from '../Type'
+
+// const { persistAtom } = recoilPersist()
+
+export const ContentData = atom<Ttable[]>({
+  key: 'ContentData',
+  default: [],
+  // effects_UNSTABLE: [persistAtom],
+})
+
+export const orderData = atom<Ttable[]>({
+  key: 'orderData',
+  default: [],
+  // effects_UNSTABLE: [persistAtom],
+})
