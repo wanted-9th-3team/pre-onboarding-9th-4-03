0d07cd93b6a8b329e5f04026fc32977a6636f967
michoball <myunghun0114@gmail.com>
2023-03-20 21:35:32 +0900

    feat: Apply react-qeury for data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import './App.css'
+import useGetTableQuery from './hooks/queries/useGetTableQuery'
+import { useAppDispatch } from './store'
+import { setTableLists } from './store/table/tableSlice'
 
 function App() {
+  const dispatch = useAppDispatch()
+  const { useGetAllTableDataQuery } = useGetTableQuery()
+  const { data: tableLists } = useGetAllTableDataQuery()
+
+  useEffect(() => {
+    if (tableLists) dispatch(setTableLists(tableLists))
+  }, [tableLists, dispatch])
+
   return (
     <BrowserRouter>
       <Routes>
