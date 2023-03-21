600559b12440602be9c0c6305b44121fc501112c
rlawodnr1109 <wodnr1109@naver.com>
2023-03-21 21:46:32 +0900

    feat: 오름차순 내림차순 기능 추가

--- src/components/sortData/index.tsx
+++ src/components/sortData/index.tsx
@@ -5,25 +5,37 @@ function SortData() {
   const [searchParams, setSearchParams] = useSearchParams()
   const filterParams = searchParams.get('filterParams')
   const searchNameParams = searchParams.get('searchNameParams')
+  const sortParams = searchParams.get('sortParams')
 
   const sortingHandler = (data: string) => {
+    let sortData = ''
+    if (sortParams === 'orderNumberDESC' && data === 'orderNumberDESC') {
+      sortData = 'orderNumberASC'
+    } else if (data === 'orderNumberDESC') {
+      sortData = 'orderNumberDESC'
+    } else if (sortParams === 'orderTimeDESC' && data === 'orderTimeDESC') {
+      sortData = 'orderTimeASC'
+    } else if (data === 'orderTimeDESC') {
+      sortData = 'orderTimeDESC'
+    }
+
     if (filterParams !== null && searchNameParams !== null) {
-      searchParams.set('sortParams', data)
+      searchParams.set('sortParams', sortData)
       searchParams.set('filterParams', filterParams)
       searchParams.set('searchNameParams', searchNameParams)
       setSearchParams(searchParams)
     } else if (filterParams !== null) {
-      searchParams.set('sortParams', data)
+      searchParams.set('sortParams', sortData)
       searchParams.set('filterParams', filterParams)
       searchParams.set('searchNameParams', '')
       setSearchParams(searchParams)
     } else if (searchNameParams !== null) {
-      searchParams.set('sortParams', data)
+      searchParams.set('sortParams', sortData)
       searchParams.set('filterParams', 'all')
       searchParams.set('searchNameParams', searchNameParams)
       setSearchParams(searchParams)
     } else {
-      searchParams.set('sortParams', data)
+      searchParams.set('sortParams', sortData)
       searchParams.set('filterParams', 'all')
       searchParams.set('searchNameParams', '')
       setSearchParams(searchParams)
@@ -33,14 +45,14 @@ function SortData() {
     <Box>
       <Button
         onClick={() => {
-          sortingHandler('orderNumber')
+          sortingHandler('orderNumberDESC')
         }}
       >
         주문번호순으로 보기
       </Button>
       <Button
         onClick={() => {
-          sortingHandler('orderTime')
+          sortingHandler('orderTimeDESC')
         }}
       >
         거래시간순으로 보기
