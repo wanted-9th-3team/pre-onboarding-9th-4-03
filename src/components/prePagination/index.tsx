ef5349bddd10aeb8771956c2fc9ccfd64e9e0cd4
skyhanull <anky0425@naver.com>
2023-03-22 03:27:10 +0900

    add:주석명 변경및 이름필터링 구현

--- src/components/prePagination/index.tsx
+++ src/components/prePagination/index.tsx
@@ -1,19 +1,20 @@
 import { useEffect, useState, useCallback } from 'react'
 import { TableContainer } from '@chakra-ui/react'
 import { useRecoilState } from 'recoil'
-// import { useQuery } from 'react-query'
-import { useParams, useSearchParams } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 import TableItem from '../TableItem'
 import { Ttable } from '../../Type'
 import Pagination from '../pagination/index'
 import { ContentData, NameData, productData } from '../../atom/index'
 
-function PrePagination({ tableData }: any) {
-  const [searchParams, setSearchParams] = useSearchParams()
-
+function PrePagination() {
+  const [searchParams] = useSearchParams() /* url query받아오기 */
   const [tableDatas, setTableDatas] =
     useRecoilState(ContentData) /* 전체데이터 */
-  const [named, setNamed] = useRecoilState(NameData)
+  const [named, setNamed] =
+    useRecoilState(
+      NameData
+    ) /* 검색한 이름 =>useRecoilValue로 하면 검색이 안되는 현상 발생? 왜일까 */
   const [product, setProduct] = useRecoilState(productData)
   const [count, setCount] = useState<number>(0) /* 아이템 총 개수 */
   const [currentPage, setCurrentPage] =
@@ -22,127 +23,59 @@ function PrePagination({ tableData }: any) {
   const indexOfLastPost = currentPage * postPerPage
   const indexOfFirstPost = indexOfLastPost - postPerPage
   const [currentPosts, setCurrentPosts] = useState<Ttable[]>()
-  const { name } = useParams()
 
   const setPage = (currentPages: number) => {
     setCurrentPage(currentPages)
   }
 
-  // function timeSortHandler() {
-  //   if (searchParams.get('sort') === 'timeAndDate') {
-  //     const filterOrderName = [...tableDatas]
-  //     // setProduct(
-
-  //     // )
-  //     const BB = filterOrderName.sort(
-  //       (a: any, b: any) =>
-  //         new Date(b.transaction_time).getTime() -
-  //         new Date(a.transaction_time).getTime()
-  //     )
-  //     return BB
-  //   }
-  //   return null
-  // }
-
   const filterHandler = useCallback(() => {
-    const filterTrue = [...tableData]
-    if (searchParams.get('boolean') === 'true') {
+    const filterTrue = [...tableDatas]
+    if (searchParams.get('status') === 'true') {
       const AK = filterTrue?.filter((item: any) => item.status === true)
       return AK
     }
-    if (searchParams.get('boolean') === 'false') {
+    if (searchParams.get('status') === 'false') {
       const BK = filterTrue?.filter((item: any) => item.status === false)
       return BK
     }
-    if (searchParams.get('boolean') === 'ALL') {
+    if (searchParams.get('status') === 'ALL') {
       const CK = filterTrue?.map(item => item)
       return CK
     }
     return filterTrue
-  }, [searchParams, tableData])
+  }, [searchParams, tableDatas])
 
-  const NameHandler = useCallback(
-    (names: string | undefined, nameparam: any) => {
-      // if (searchParams.get('name') === nameparam) {
-      //   const filterOrderName = [...tableDatas]
-      //   const KK = filterOrderName?.filter(
-      //     item => item.customer_name === nameparam
-      //   )
+  const NameHandler = useCallback(() => {
+    const statusfiltering = filterHandler()
 
-      //   setProduct(KK)
-      // }
-      // sortIdHandler()
-      // if (searchParams.get('sort') === 'id') {
-      //   const filterOrderName = [...tableDatas]
-      //   const AAA = filterOrderName.sort((a: any, b: any) => b.id - a.id)
-      //   setProduct(AAA)
-      // }
-      // const KK = sortIdHandler()
-      // console.log(KK)
-      // const LL = timeSortHandler()
-      // console.log(LL)
-      const JJ = filterHandler()
-      console.log(JJ)
-
-      /* sort의 값중 id나 time~이 있으면 sort을 하고 
+    /* sort의 값중 id나 time~이 있으면 sort을 하고 
       만약 true 또는 flase라면 또 필터링 분기가 총 4개 존재 */
-      const filterDataed = [...tableDatas]
-      const AAAAA = searchParams.get('boolean') ? JJ : filterDataed
-      const DDDDD =
-        searchParams.get('sort') === 'id'
-          ? AAAAA.sort((a, b) => b.id - a.id)
-          : searchParams.get('sort') === 'timeAndDate'
-          ? AAAAA.sort(
-              (a, b) =>
-                new Date(b.transaction_time).getTime() -
-                new Date(a.transaction_time).getTime()
-            )
-          : AAAAA
+    const filterDataed = [...tableDatas]
+    const statusData = searchParams.get('status')
+      ? statusfiltering
+      : filterDataed
 
-      console.log(DDDDD)
-      setProduct(DDDDD)
-      // const sortString = searchParams.get('sort')
-      // const booleanString: string = searchParams.get('boolean')
-      // sortString === id ? KK : LL
-      // const ASD = sortString === 'id' ? KK : LL
-      // if (searchParams.get('sort') && searchParams.get('boolean')) {
-      //   // const arr2 = KK.filter(x1 => JJ.some(x2 => x1.status === x2.idx))
-      //   // const ASD = sortString === 'id' ? KK : LL
-      //   // const arr2 = ASD?.filter(el => el.status === booleanString)
-      // }
+    const sortData =
+      searchParams.get('sort') === 'id'
+        ? statusData.sort((a, b) => b.id - a.id)
+        : searchParams.get('sort') === 'timeAndDate'
+        ? statusData.sort(
+            (a, b) =>
+              new Date(b.transaction_time).getTime() -
+              new Date(a.transaction_time).getTime()
+          )
+        : statusData
 
-      // if (names === 'filter') {
-      //   const filterTrue = [...tableData]
-      //   const filterData =
-      //     searchParams.get('boolean') === 'true'
-      //       ? filterTrue?.filter((item: any) => item.status === true)
-      //       : searchParams.get('boolean') === 'false'
-      //       ? filterTrue?.filter((item: any) => item.status === false)
-      //       : searchParams.get('boolean') === 'ALL'
-      //       ? filterTrue?.map(item => item)
-      //       : filterTrue /* 기본주소가 /가 안되는 중... ㅎ */
-      //   setProduct(filterData)
-      // }
-      // if (searchParams.get('sort') === 'timeAndDate') {
-      //   const filterOrderName = [...tableDatas]
-      //   setProduct(
-      //     filterOrderName.sort(
-      //       (a: any, b: any) =>
-      //         new Date(b.transaction_time).getTime() -
-      //         new Date(a.transaction_time).getTime()
-      //     )
-      //   )
-      // }
-      // if (names === undefined) {
-      //   setProduct(tableData)
-      // }
-    },
-    [filterHandler, searchParams, setProduct, tableDatas]
-  )
+    const filteredALL = searchParams.get('name')?.toString()
+      ? sortData.filter(list => list.customer_name === named)
+      : sortData
+
+    setProduct(filteredALL)
+  }, [filterHandler, named, searchParams, setProduct, tableDatas])
 
   useEffect(() => {
-    NameHandler(name, named)
-  }, [NameHandler, name, named])
+    NameHandler()
+  }, [NameHandler])
 
   useEffect(() => {
     const K = product ? product?.length : 0
@@ -155,6 +88,7 @@ function PrePagination({ tableData }: any) {
       <TableContainer>
         <TableItem tableData={currentPosts!} />
       </TableContainer>
+
       <span>
         <Pagination
           setPage={setPage}
