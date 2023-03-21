a5dd61240284c3b6b48c7f00d62082663f70772a
skyhanull <anky0425@naver.com>
2023-03-21 13:59:58 +0900

    refactor:버튼대신 토글로 정렬할 수 있도록 구현

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,15 +1,6 @@
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  Select,
-} from '@chakra-ui/react'
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Table, Thead, Tbody, Tr, Th, Td, Select } from '@chakra-ui/react'
+import { useCallback, useEffect, useState } from 'react'
+import { Link, useNavigate, useSearchParams } from 'react-router-dom'
 import { Ttable } from '../../Type'
 
 interface ITableList {
@@ -19,30 +10,57 @@ interface ITableList {
 function TableItem({ tableData }: ITableList) {
   const [sortName, setSortName] = useState('오름차순')
   const [timeName, setTimeName] = useState('오름차순')
+  const [trueName, setTrueName] = useState('ALL')
+  const [searchParams, setSearchParams] = useSearchParams()
   const nameArray = ['오름차순', '내림차순']
-  const idurl = 'sort'
-  const timeurl = 'timeAndDate'
+  // const idurl = 'sort'
+  // const timeurl = 'timeAndDate'
   const navigate = useNavigate()
+
+  const sortname = searchParams.get('sort')
+  const timename = searchParams.get('time')
+  const booleanName = searchParams.get('boolean')
+  const Nameparam = searchParams.get('name')
 
   const orderChangeHandler = (value: any) => {
     if (value === '오름차순') {
-      navigate(`/sort`)
+      navigate(`/filter?sort=id`)
+      searchParams.set('sort', 'id')
       setSortName('내림차순')
     } else {
       navigate('/')
       setSortName('오름차순')
+      searchParams.set('sort', '')
     }
   }
 
   const timeChangeHandler = (value: any) => {
     if (value === '오름차순') {
-      navigate(`/timeAndDate`)
+      navigate(`/filter?sort=timeAndDate`)
+      searchParams.set('sorts', 'timeAndDate')
       setTimeName('내림차순')
     } else {
       navigate('/')
+      searchParams.set('time', '')
       setTimeName('오름차순')
     }
   }
+
+  const booleanNameHandler = (e: any) => {
+    setTrueName(e.target.value)
+  }
+
+  const TrueChangeHandler = useCallback(() => {
+    if (trueName === 'ALL') {
+      navigate(`/filter`)
+    } else {
+      navigate(`/filter?sort=${sortname}&boolean=${trueName}`)
+    }
+  }, [navigate, sortname, trueName])
+
+  useEffect(() => {
+    TrueChangeHandler()
+  }, [TrueChangeHandler])
 
   return (
     <Table variant="striped" colorScheme="teal">
@@ -50,12 +68,9 @@ function TableItem({ tableData }: ITableList) {
         <Tr>
           <Th>
             <Select
-              // placeholder="주문번호"
               onChange={() => orderChangeHandler(sortName)}
               value={sortName}
             >
-              {/* <option value="오름차순">오름차순</option>
-            <option value="내림차순">내림차순</option> */}
               {nameArray.map(item => (
                 <option value={item} key={item}>
                   {item}
@@ -66,7 +81,17 @@ function TableItem({ tableData }: ITableList) {
           <Th>가격</Th>
           <Th>고객 번호</Th>
           <Th>고객 이름</Th>
-          <Th>주문처리 상태</Th>
+          <Th>
+            <Select
+              placeholder="주문번호"
+              onChange={booleanNameHandler}
+              value={trueName}
+            >
+              <option value="ALL">ALL</option>
+              <option value="true">true</option>
+              <option value="false">false</option>
+            </Select>
+          </Th>
           <Th>
             <Select
               // placeholder="주문번호"
