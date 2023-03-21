d3a5ddf045539465db3f974308b009d97e1b3d55
skyhanull <anky0425@naver.com>
2023-03-21 17:20:35 +0900

    refactor: 필터링 수정

--- src/components/prePagination/index.tsx
+++ src/components/prePagination/index.tsx
@@ -6,17 +6,15 @@ import { useParams, useSearchParams } from 'react-router-dom'
 import TableItem from '../TableItem'
 import { Ttable } from '../../Type'
 import Pagination from '../pagination/index'
-import { ContentData } from '../../atom/index'
+import { ContentData, NameData, productData } from '../../atom/index'
 
 function PrePagination({ tableData }: any) {
   const [searchParams, setSearchParams] = useSearchParams()
 
   const [tableDatas, setTableDatas] =
     useRecoilState(ContentData) /* 전체데이터 */
-
-  console.log(tableDatas)
-
-  const [product, setProduct] = useState<Ttable[]>()
+  const [named, setNamed] = useRecoilState(NameData)
+  const [product, setProduct] = useRecoilState(productData)
   const [count, setCount] = useState<number>(0) /* 아이템 총 개수 */
   const [currentPage, setCurrentPage] =
     useState<number>(1) /* 현재 페이지. default 값으로 1 */
@@ -30,24 +28,48 @@ function PrePagination({ tableData }: any) {
     setCurrentPage(currentPages)
   }
 
+  function sortIdHandler() {
+    if (searchParams.get('sort') === 'id') {
+      const filterOrderName = [...tableDatas]
+      const AAA = filterOrderName.sort((a: any, b: any) => {
+        return b.id - a.id
+      })
+      // setProduct(AAA)
+      return AAA
+    }
+    return null
+  }
+
   const NameHandler = useCallback(
-    (names: string | undefined) => {
-      if (names === 'sort') {
-        const filterOrderName = [...tableDatas]
-        setProduct(filterOrderName.sort((a: any, b: any) => b.id - a.id))
-      }
-      // if (names === 'name') {
-      //   setProduct(tableNameDatas)
+    (names: string | undefined, nameparam: any) => {
+      // if (searchParams.get('name') === nameparam) {
+      //   const filterOrderName = [...tableDatas]
+      //   const KK = filterOrderName?.filter(
+      //     item => item.customer_name === nameparam
+      //   )
+
+      //   setProduct(KK)
       // }
+      // sortIdHandler()
+      if (searchParams.get('sort') === 'id') {
+        const filterOrderName = [...tableDatas]
+        const AAA = filterOrderName.sort((a: any, b: any) => b.id - a.id)
+        setProduct(AAA)
+      }
+
       if (names === 'filter') {
         const filterTrue = [...tableData]
         const filterData =
-          searchParams.get('boolean') === 'truely'
+          searchParams.get('boolean') === 'true'
             ? filterTrue?.filter((item: any) => item.status === true)
-            : filterTrue?.filter((item: any) => item.status === false)
+            : searchParams.get('boolean') === 'false'
+            ? filterTrue?.filter((item: any) => item.status === false)
+            : searchParams.get('boolean') === 'ALL'
+            ? filterTrue?.map(item => item)
+            : filterTrue /* 기본주소가 /가 안되는 중... ㅎ */
         setProduct(filterData)
       }
-      if (names === 'timeAndDate') {
+      if (searchParams.get('sort') === 'timeAndDate') {
         const filterOrderName = [...tableDatas]
         setProduct(
           filterOrderName.sort(
@@ -57,16 +79,16 @@ function PrePagination({ tableData }: any) {
           )
         )
       }
-      if (names === undefined) {
-        setProduct(tableData)
-      }
+      // if (names === undefined) {
+      //   setProduct(tableData)
+      // }
     },
-    [searchParams, tableData, tableDatas]
+    [searchParams, setProduct, tableData, tableDatas]
   )
 
   useEffect(() => {
-    NameHandler(name)
-  }, [NameHandler, name])
+    NameHandler(name, named)
+  }, [NameHandler, name, named])
 
   useEffect(() => {
     const K = product ? product?.length : 0
