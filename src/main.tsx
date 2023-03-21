25a48504e079240304f5d8da8466bb8ec7962cec
michoball <myunghun0114@gmail.com>
2023-03-22 04:18:40 +0900

    refactor: 필요없는 코드 전부 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import './index.css'
-import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -15,11 +13,9 @@ const queryClient = new QueryClient()
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </Provider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
