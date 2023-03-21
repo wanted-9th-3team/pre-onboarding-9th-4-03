bc96946ceb99e06d247d6c21990458dc6fd1a372
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2023-03-21 18:39:33 +0900

    feat : 페이지에 데이터 5초마다 받아오기

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,21 @@
+import { useQuery } from 'react-query'
+import getTradeInfo from '../../apis/TableApi'
+import { filterTradeByDate } from '../../utils/filter'
+import TradeTable from '../../components/TradeTable'
+
+const TODAY = '2023-03-08'
 function Home() {
-  return <h1>Hello World</h1>
+  const { data } = useQuery({
+    queryKey: ['getTrade'],
+    queryFn: getTradeInfo,
+    refetchInterval: 5000,
+  })
+
+  return (
+    <div>
+      <TradeTable trade={filterTradeByDate(data, TODAY)} />
+    </div>
+  )
 }
 
 export default Home
