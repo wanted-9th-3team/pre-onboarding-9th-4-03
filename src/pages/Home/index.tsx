0b0351b3f194a434be4bc91e9a874110c6358347
rlawodnr1109 <wodnr1109@naver.com>
2023-03-21 21:47:45 +0900

    feat: 세 기능 연동, setInterval 사용 5초마다 데이터 받아오기 구현.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,38 +28,47 @@ function Home() {
 
   const [orderData, setOrderData] = useState<IOrderData[]>(orderDataBase)
 
+  const [notFilterData, setNotFilterData] = useState<IOrderData[]>(orderData)
+
+  const [notSearchData, setNotSearchData] = useState<IOrderData[]>(orderData)
+
+  const [orderViewData, setOrderViewData] = useState<IOrderData[]>(orderData)
+
   const getDataBase = async () => {
     const data = await getOrderInfo()
     if (data) {
-      setOrderDataBase(
-        data
-          .filter((DataItem: IOrderData) => {
-            const DateData = new Date(DataItem.transaction_time)
-            return (
-              DateData.getFullYear() === 2023 &&
-              DateData.getDate() === 8 &&
-              DateData.getMonth() === 2
-            )
-          })
-          .sort(function sortASC(a: IOrderData, b: IOrderData) {
-            if (a.id > b.id) {
-              return 1
-            }
-            if (a.id < b.id) {
-              return -1
-            }
-            return 0
-          })
-      )
+      setOrderDataBase(data)
     }
   }
 
-  useEffect(() => {
-    getDataBase()
-  }, [])
+  setInterval(getDataBase, 5000)
 
-  const [products, setProducts] = useState<IOrderData[]>(orderData)
-  const [totalCount, setTotalCount] = useState(orderData.length)
+  useEffect(() => {
+    if (orderDataBase.length !== 0) {
+      const DATABASE = orderDataBase
+        .filter((DataItem: IOrderData) => {
+          const DateData = new Date(DataItem.transaction_time)
+          return (
+            DateData.getFullYear() === 2023 &&
+            DateData.getDate() === 8 &&
+            DateData.getMonth() === 2
+          )
+        })
+        .sort(function sortASC(a: IOrderData, b: IOrderData) {
+          if (a.id > b.id) {
+            return 1
+          }
+          if (a.id < b.id) {
+            return -1
+          }
+          return 0
+        })
+      setOrderData(DATABASE)
+    }
+  }, [orderDataBase])
+
+  const [products, setProducts] = useState<IOrderData[]>(orderViewData)
+  const [totalCount, setTotalCount] = useState(orderViewData.length)
   const [currentPage, setCurrentPage] = useState(1)
   const [postPerPage] = useState(50)
   const [indexOfLastPost, setIndexOfLastPost] = useState(0)
@@ -67,7 +76,7 @@ function Home() {
   const [currentPosts, setCurrentPosts] = useState<IOrderData[]>([])
 
   useEffect(() => {
-    setProducts(orderData)
+    setProducts(orderViewData)
     setTotalCount(products.length)
     setIndexOfLastPost(currentPage * postPerPage)
     setIndexOfFirstPost(indexOfLastPost - postPerPage)
@@ -78,35 +87,17 @@ function Home() {
     indexOfFirstPost,
     products,
     postPerPage,
-    orderData,
+    orderViewData,
   ])
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page)
   }
 
-  const filterHandler = (data: string) => {
-    if (data === 'O') {
+  const sortingHandler = (data: string | null) => {
+    if (data === 'orderNumberDESC') {
       setOrderData(
-        orderDataBase.filter((DataItem: IOrderData) => {
-          return DataItem.status === true
-        })
-      )
-    } else if (data === 'X') {
-      setOrderData(
-        orderDataBase.filter((DataItem: IOrderData) => {
-          return DataItem.status === false
-        })
-      )
-    } else if (data === 'all') {
-      setOrderData(orderDataBase)
-    }
-  }
-
-  const sortingHandler = (data: string) => {
-    if (data === 'orderNumber') {
-      setOrderData(
-        orderDataBase.sort(function sortDESC(a: IOrderData, b: IOrderData) {
+        orderData.sort(function sortDESC(a: IOrderData, b: IOrderData) {
           if (a.id < b.id) {
             return 1
           }
@@ -116,9 +107,21 @@ function Home() {
           return 0
         })
       )
-    } else if (data === 'orderTime') {
+    } else if (data === 'orderNumberASC') {
       setOrderData(
-        orderDataBase.sort(function sortDESC(a: IOrderData, b: IOrderData) {
+        orderData.sort(function sortDESC(a: IOrderData, b: IOrderData) {
+          if (a.id > b.id) {
+            return 1
+          }
+          if (a.id < b.id) {
+            return -1
+          }
+          return 0
+        })
+      )
+    } else if (data === 'orderTimeDESC') {
+      setOrderData(
+        orderData.sort(function sortDESC(a: IOrderData, b: IOrderData) {
           if (a.transaction_time < b.transaction_time) {
             return 1
           }
@@ -128,9 +131,21 @@ function Home() {
           return 0
         })
       )
+    } else if (data === 'orderTimeASC') {
+      setOrderData(
+        orderData.sort(function sortDESC(a: IOrderData, b: IOrderData) {
+          if (a.transaction_time > b.transaction_time) {
+            return 1
+          }
+          if (a.transaction_time < b.transaction_time) {
+            return -1
+          }
+          return 0
+        })
+      )
     } else if (data === 'sortASC') {
       setOrderData(
-        orderDataBase.sort(function sortASC(a: IOrderData, b: IOrderData) {
+        orderData.sort(function sortASC(a: IOrderData, b: IOrderData) {
           if (a.id > b.id) {
             return 1
           }
@@ -140,15 +155,47 @@ function Home() {
           return 0
         })
       )
-    } else {
-      setOrderData(orderData)
+    }
+  }
+
+  const filterHandler = (data: string | null) => {
+    if (data === 'O') {
+      setOrderViewData(
+        notFilterData.filter((DataItem: IOrderData) => {
+          return DataItem.status === true
+        })
+      )
+      setNotSearchData(
+        orderData.filter((DataItem: IOrderData) => {
+          return DataItem.status === true
+        })
+      )
+    } else if (data === 'X') {
+      setOrderViewData(
+        notFilterData.filter((DataItem: IOrderData) => {
+          return DataItem.status === false
+        })
+      )
+      setNotSearchData(
+        orderData.filter((DataItem: IOrderData) => {
+          return DataItem.status === false
+        })
+      )
+    } else if (data === 'all') {
+      setOrderViewData(notFilterData)
+      setNotSearchData(orderData)
     }
   }
 
   const searchNameHandler = () => {
     if (searchNameParams !== null) {
-      setOrderData(
-        orderDataBase.filter((DataItem: IOrderData) => {
+      setOrderViewData(
+        notSearchData.filter((DataItem: IOrderData) => {
+          return DataItem.customer_name.includes(searchNameParams)
+        })
+      )
+      setNotFilterData(
+        orderData.filter((DataItem: IOrderData) => {
           return DataItem.customer_name.includes(searchNameParams)
         })
       )
@@ -156,26 +203,45 @@ function Home() {
   }
 
   useEffect(() => {
-    if (filterParams !== null) {
-      filterHandler(filterParams)
-    } else {
-      console.log('filterParams is null')
-    }
-    if (sortParams !== null) {
-      sortingHandler(sortParams)
-    } else {
-      console.log('sortParams is null')
-    }
-    if (searchNameParams !== null) {
-      searchNameHandler()
-    } else {
-      console.log('searchNameParams is null')
+    filterHandler(filterParams)
+    sortingHandler(sortParams)
+    searchNameHandler()
+  }, [orderData, filterParams, sortParams, searchNameParams])
+
+  const sortingOnClickHandler = (data: string) => {
+    let sortData = ''
+    if (sortParams === 'orderNumberDESC' && data === 'orderNumberDESC') {
+      sortData = 'orderNumberASC'
+    } else if (data === 'orderNumberDESC') {
+      sortData = 'orderNumberDESC'
+    } else if (sortParams === 'orderTimeDESC' && data === 'orderTimeDESC') {
+      sortData = 'orderTimeASC'
+    } else if (data === 'orderTimeDESC') {
+      sortData = 'orderTimeDESC'
     }
 
-    console.log(filterParams)
-    console.log(sortParams)
-    console.log(searchNameParams)
-  }, [searchParams])
+    if (filterParams !== null && searchNameParams !== null) {
+      searchParams.set('sortParams', sortData)
+      searchParams.set('filterParams', filterParams)
+      searchParams.set('searchNameParams', searchNameParams)
+      setSearchParams(searchParams)
+    } else if (filterParams !== null) {
+      searchParams.set('sortParams', sortData)
+      searchParams.set('filterParams', filterParams)
+      searchParams.set('searchNameParams', '')
+      setSearchParams(searchParams)
+    } else if (searchNameParams !== null) {
+      searchParams.set('sortParams', sortData)
+      searchParams.set('filterParams', 'all')
+      searchParams.set('searchNameParams', searchNameParams)
+      setSearchParams(searchParams)
+    } else {
+      searchParams.set('sortParams', sortData)
+      searchParams.set('filterParams', 'all')
+      searchParams.set('searchNameParams', '')
+      setSearchParams(searchParams)
+    }
+  }
 
   return (
     <Box>
@@ -186,8 +252,20 @@ function Home() {
         <Table size="lg">
           <Thead>
             <Tr>
-              <Th>주문번호</Th>
-              <Th>거래시간</Th>
+              <Th
+                onClick={() => {
+                  sortingOnClickHandler('orderNumberDESC')
+                }}
+              >
+                주문번호
+              </Th>
+              <Th
+                onClick={() => {
+                  sortingOnClickHandler('orderTimeDESC')
+                }}
+              >
+                거래시간
+              </Th>
               <Th>고객번호</Th>
               <Th>고객이름</Th>
               <Th>가격</Th>
