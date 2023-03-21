c943446b866656f0437470e88ed4a96b93574367
skyhanull <anky0425@naver.com>
2023-03-21 18:07:52 +0900

    refactor: 다중 필터링이 가능하게 구현

--- src/components/prePagination/index.tsx
+++ src/components/prePagination/index.tsx
@@ -28,17 +28,38 @@ function PrePagination({ tableData }: any) {
     setCurrentPage(currentPages)
   }
 
-  function sortIdHandler() {
-    if (searchParams.get('sort') === 'id') {
-      const filterOrderName = [...tableDatas]
-      const AAA = filterOrderName.sort((a: any, b: any) => {
-        return b.id - a.id
-      })
-      // setProduct(AAA)
-      return AAA
+  // function timeSortHandler() {
+  //   if (searchParams.get('sort') === 'timeAndDate') {
+  //     const filterOrderName = [...tableDatas]
+  //     // setProduct(
+
+  //     // )
+  //     const BB = filterOrderName.sort(
+  //       (a: any, b: any) =>
+  //         new Date(b.transaction_time).getTime() -
+  //         new Date(a.transaction_time).getTime()
+  //     )
+  //     return BB
+  //   }
+  //   return null
+  // }
+
+  const filterHandler = useCallback(() => {
+    const filterTrue = [...tableData]
+    if (searchParams.get('boolean') === 'true') {
+      const AK = filterTrue?.filter((item: any) => item.status === true)
+      return AK
     }
-    return null
-  }
+    if (searchParams.get('boolean') === 'false') {
+      const BK = filterTrue?.filter((item: any) => item.status === false)
+      return BK
+    }
+    if (searchParams.get('boolean') === 'ALL') {
+      const CK = filterTrue?.map(item => item)
+      return CK
+    }
+    return filterTrue
+  }, [searchParams, tableData])
 
   const NameHandler = useCallback(
     (names: string | undefined, nameparam: any) => {
@@ -51,39 +72,72 @@ function PrePagination({ tableData }: any) {
       //   setProduct(KK)
       // }
       // sortIdHandler()
-      if (searchParams.get('sort') === 'id') {
-        const filterOrderName = [...tableDatas]
-        const AAA = filterOrderName.sort((a: any, b: any) => b.id - a.id)
-        setProduct(AAA)
-      }
+      // if (searchParams.get('sort') === 'id') {
+      //   const filterOrderName = [...tableDatas]
+      //   const AAA = filterOrderName.sort((a: any, b: any) => b.id - a.id)
+      //   setProduct(AAA)
+      // }
+      // const KK = sortIdHandler()
+      // console.log(KK)
+      // const LL = timeSortHandler()
+      // console.log(LL)
+      const JJ = filterHandler()
+      console.log(JJ)
 
-      if (names === 'filter') {
-        const filterTrue = [...tableData]
-        const filterData =
-          searchParams.get('boolean') === 'true'
-            ? filterTrue?.filter((item: any) => item.status === true)
-            : searchParams.get('boolean') === 'false'
-            ? filterTrue?.filter((item: any) => item.status === false)
-            : searchParams.get('boolean') === 'ALL'
-            ? filterTrue?.map(item => item)
-            : filterTrue /* 기본주소가 /가 안되는 중... ㅎ */
-        setProduct(filterData)
-      }
-      if (searchParams.get('sort') === 'timeAndDate') {
-        const filterOrderName = [...tableDatas]
-        setProduct(
-          filterOrderName.sort(
-            (a: any, b: any) =>
-              new Date(b.transaction_time).getTime() -
-              new Date(a.transaction_time).getTime()
-          )
-        )
-      }
+      /* sort의 값중 id나 time~이 있으면 sort을 하고 
+      만약 true 또는 flase라면 또 필터링 분기가 총 4개 존재 */
+      const filterDataed = [...tableDatas]
+      const AAAAA = searchParams.get('boolean') ? JJ : filterDataed
+      const DDDDD =
+        searchParams.get('sort') === 'id'
+          ? AAAAA.sort((a, b) => b.id - a.id)
+          : searchParams.get('sort') === 'timeAndDate'
+          ? AAAAA.sort(
+              (a, b) =>
+                new Date(b.transaction_time).getTime() -
+                new Date(a.transaction_time).getTime()
+            )
+          : AAAAA
+
+      console.log(DDDDD)
+      setProduct(DDDDD)
+      // const sortString = searchParams.get('sort')
+      // const booleanString: string = searchParams.get('boolean')
+      // sortString === id ? KK : LL
+      // const ASD = sortString === 'id' ? KK : LL
+      // if (searchParams.get('sort') && searchParams.get('boolean')) {
+      //   // const arr2 = KK.filter(x1 => JJ.some(x2 => x1.status === x2.idx))
+      //   // const ASD = sortString === 'id' ? KK : LL
+      //   // const arr2 = ASD?.filter(el => el.status === booleanString)
+      // }
+
+      // if (names === 'filter') {
+      //   const filterTrue = [...tableData]
+      //   const filterData =
+      //     searchParams.get('boolean') === 'true'
+      //       ? filterTrue?.filter((item: any) => item.status === true)
+      //       : searchParams.get('boolean') === 'false'
+      //       ? filterTrue?.filter((item: any) => item.status === false)
+      //       : searchParams.get('boolean') === 'ALL'
+      //       ? filterTrue?.map(item => item)
+      //       : filterTrue /* 기본주소가 /가 안되는 중... ㅎ */
+      //   setProduct(filterData)
+      // }
+      // if (searchParams.get('sort') === 'timeAndDate') {
+      //   const filterOrderName = [...tableDatas]
+      //   setProduct(
+      //     filterOrderName.sort(
+      //       (a: any, b: any) =>
+      //         new Date(b.transaction_time).getTime() -
+      //         new Date(a.transaction_time).getTime()
+      //     )
+      //   )
+      // }
       // if (names === undefined) {
       //   setProduct(tableData)
       // }
     },
-    [searchParams, setProduct, tableData, tableDatas]
+    [filterHandler, searchParams, setProduct, tableDatas]
   )
 
   useEffect(() => {
