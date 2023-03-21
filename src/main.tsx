eb06b039bdb4bc0c18a64abeaa4e22333b51ff02
bona <bona373737@gmail.com>
2023-03-21 18:25:38 +0900

    feat: customer_name기준 검색필터

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,63 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 import { Table } from 'antd'
 import type { ColumnsType, TableProps } from 'antd/es/table'
 import { BiSearch } from 'react-icons/bi'
 import { GrPowerReset } from 'react-icons/gr'
+import { useQuery } from 'react-query'
 import getOrderList from '../../apis/getOrderList'
 import { TOrderInfo } from '../../Type'
 
 const HomeContainer = styled.div`
   box-sizing: border-box;
   padding: 20px 30px;
+
+  .menu {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .search_form {
+      margin: 0 50px;
+      display: flex;
+      align-items: center;
+      input {
+        height: 22px;
+        text-indent: 5px;
+        border-radius: 5px;
+        outline: none;
+        border: 0.3px solid #dddddd;
+      }
+      button {
+        background-color: transparent;
+        border: none;
+        font-size: 18px;
+        cursor: pointer;
+        :hover {
+          color: blue;
+        }
+      }
+      .search_btn {
+        position: relative;
+        right: 30px;
+      }
+      .reset_btn {
+        position: relative;
+        right: 28px;
+      }
+    }
+  }
 `
 
 function Home() {
   const navigate = useNavigate()
   const location = useLocation()
-  const [orderList, setOrderList] = useState<TOrderInfo[] | null>()
-  const [searchKeyWord, setSearchKeyWord] = useState<string>()
+  const searchInput = useRef('')
+  const [searchKeyWord, setSearchKeyWord] = useState<string>('')
 
-  useEffect(() => {
-    getOrderList().then(data => {
-      setOrderList(data)
-    })
-  }, [])
+  const query = useQuery('getOrderList', getOrderList, {
+    refetchInterval: 5000,
+  })
 
   const columns = [
     {
@@ -33,6 +67,8 @@ function Home() {
         return a.id > b.id
       },
       sortDirections: ['descend'],
+      align: 'center',
+      defaultSortOrder: 'ascend',
     },
     {
       title: 'transaction_time',
@@ -42,6 +78,7 @@ function Home() {
         return new Date(a.transaction_time) > new Date(b.transaction_time)
       },
       sortDirections: ['descend'],
+      align: 'center',
     },
     {
       title: 'status',
@@ -62,18 +99,29 @@ function Home() {
       onFilter: (value: string, record: TOrderInfo): boolean => {
         return !!value === record.status
       },
+      align: 'center',
     },
     {
       title: 'customer_id',
       dataIndex: 'customer_id',
+      align: 'center',
     },
     {
       title: 'customer_name',
       dataIndex: 'customer_name',
+      align: 'center',
+      filteredValue: [searchKeyWord],
+      onFilter: (value: string, record: TOrderInfo): boolean => {
+        return record.customer_name
+          .toString()
+          .toLowerCase()
+          .includes((value as string).toLowerCase())
+      },
     },
     {
       title: 'currency',
       dataIndex: 'currency',
+      align: 'center',
     },
   ]
 
@@ -97,29 +145,51 @@ function Home() {
     }
 
     setSearchKeyWord(keyword)
-    const queryString = location.search.slice(
-      0,
-      location.search.indexOf('search')
-    )
-    navigate(`${queryString}search=${keyword}`)
+
+    if (location.search) {
+      const queryString = location.search.slice(
+        0,
+        location.search.indexOf('search')
+      )
+      navigate(`/${queryString}search=${keyword}`)
+    }
+  }
+
+  const onClickReset = e => {
+    setSearchKeyWord('')
+    searchInput.current.value = ''
+    if (location.search) {
+      const queryString = location.search.slice(
+        0,
+        location.search.indexOf('search')
+      )
+      navigate(`/${queryString}search=null`)
+    }
   }
 
   return (
     <HomeContainer>
-      <h1>스위치온_기업과제</h1>
-      <form className="search_input" onSubmit={onClickSearchBtn}>
-        <label htmlFor="username" />
-        <input id="username" type="text" placeholder="customer name" />
-        <button type="submit">
-          <BiSearch />
-        </button>
-        <span>
-          <GrPowerReset />
-        </span>
-      </form>
+      <div className="menu">
+        <h1>스위치온_기업과제</h1>
+        <form className="search_form" onSubmit={onClickSearchBtn}>
+          <label htmlFor="username" />
+          <input
+            id="username"
+            type="text"
+            ref={searchInput}
+            placeholder="customer name"
+          />
+          <button type="submit" className="search_btn">
+            <BiSearch />
+          </button>
+          <button type="button" className="reset_btn" onClick={onClickReset}>
+            <GrPowerReset />
+          </button>
+        </form>
+      </div>
       <Table
         columns={columns}
-        dataSource={orderList}
+        dataSource={query.data}
         pagination={{
           defaultPageSize: 50,
           showSizeChanger: false,
