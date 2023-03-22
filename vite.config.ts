0012ee36564d7fb4b496be3db51b1a66a0c92ede
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2023-03-22 18:24:33 +0900

    feat : alias path 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,36 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@hooks',
+        replacement: path.resolve(__dirname, 'src/hooks'),
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages'),
+      },
+      {
+        find: '@apis',
+        replacement: path.resolve(__dirname, 'src/apis'),
+      },
+      {
+        find: '@utils',
+        replacement: path.resolve(__dirname, 'src/utils'),
+      },
+    ],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
